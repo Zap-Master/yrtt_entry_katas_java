274ced5f698ff0edf833e4de2f8fdd0fac2201c6
Zap-Master <51678529+Zap-Master@users.noreply.github.com>
2020-11-15 14:57:00 +0000

    Update Exercise004.java

--- src/main/java/com/techreturners/exercise004/Exercise004.java
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -6,7 +6,34 @@ public class Exercise004 {
     // Leave punctuation marks untouched.
 
     public String pigLatin(String str) {
-        // Add your code here
-        return "";
+        String res = "";
+        String strPuncMarks = ",.!?() ";
+        char[] strAsArray  = str.toCharArray();
+        int firstLetterPos = 0;
+        boolean isChanged = false;
+        for (int i = 0; i < strAsArray.length; i ++){
+            if (strPuncMarks.contains(Character.toString(strAsArray[i])) && isChanged == false) {
+
+                for (int j = firstLetterPos + 1; j < i; j++){
+                    res = res + strAsArray[j];
+                }
+                res = (res + strAsArray[firstLetterPos] + "ay" + strAsArray[i]).trim();
+                isChanged = true;
+            }
+            if (Character.toString(strAsArray[i]).trim().length() == 0){
+
+                firstLetterPos = i +  1;
+                res = res + " ";
+                isChanged = false;
+            }
+            if ((i == strAsArray.length -1) && !(strPuncMarks.contains(Character.toString(strAsArray[i])))){
+                for (int k = firstLetterPos + 1; k <= i; k++){
+                    res = res + strAsArray[k];
+                }
+                res = res + strAsArray[firstLetterPos] + "ay";
+
+            }
+        }
+        return res;
     }
 }
