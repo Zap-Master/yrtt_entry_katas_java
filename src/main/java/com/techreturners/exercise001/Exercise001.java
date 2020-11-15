d37da98ac5822682f56ec82cd04bb66ea84ba1e9
Zap-Master <51678529+Zap-Master@users.noreply.github.com>
2020-11-15 13:55:16 +0000

    Update Exercise001.java

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -9,9 +9,63 @@ public class Exercise001 {
     // More examples in exercise001 test cases.
 
     // Good luck!
+   private static int partition(int[] arr, int l, int r) {
+        int t = 0;
+        int pi = r; // pivot index
+        int p = arr[r];
+        int i = l;
+        boolean f= false;
+        for (int j = l; j <  r; j ++) {
+            if (arr[j]<p  ) {
+                f = true;
+                t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
+                i++;
+            }
+        }
+        if (f || p < arr[l]) {
+            t = arr[i];
+            arr[i] = arr[r];
+            arr[r] = t;
+        }
+        pi = i;
+        return pi;
+    }
+    //QuickSort
+    private static int QuickSort(int[] arr, int low, int hi){
+        if (low < hi){
+            int pi = partition(arr, low, hi);
+            QuickSort(arr, low, pi - 1);
+            QuickSort(arr, pi + 1, hi);
+        }
+        return 0;
+    }
 
     public int singles(int [] arr){
-        // Add your code here!
-        return 0;
+        /*
+        */
+        int i = 0;
+        int s = 0;
+        QuickSort(arr, 0,arr.length - 1);
+        while (i <=arr.length - 1) {
+            if (i + 1 > arr.length -1) {
+                if (arr[i] != arr[i-1]) {
+                    s = s + arr[i];
+                    i ++;
+                }
+            }
+            else {
+                if (arr[i] != arr[i+1]) {
+                    s = s + arr[i];
+                    i ++;
+                }
+                else {
+                    i = i + 2;
+                }
+            }
+        }
+        return s;
+ 
     }
 }
